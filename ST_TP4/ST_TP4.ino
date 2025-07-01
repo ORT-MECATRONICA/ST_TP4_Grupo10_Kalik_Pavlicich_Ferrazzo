93e616bb7115bdabc8b8fc5364b01e91f98ab953
MartinK-27 <48522161@est.ort.edu.ar>
2025-07-01 11:51:26 -0300

    comentarios

--- ST_TP4/ST_TP4.ino
+++ ST_TP4/ST_TP4.ino
@@ -1,23 +1,26 @@
 //Grupo10_Kalik_Pavlicich_Ferrazzo
 
+//Librerias
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include "DHT.h"
 #include <Preferences.h>
 
+//Eeprom
 Preferences preferences;
 
+//Ajustes de pantalla
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
-
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
+//Ajustes de DHT
 #define DHTPIN 23      // Pin
 #define DHTTYPE DHT11  // tipo de dht
-
 DHT dht(DHTPIN, DHTTYPE);
 
+//Maquina de estados
 #define PANTALLA1 1
 #define ESTADO_CONFIRMACION1 2
 #define ESTADO_CONFIRMACION2 3
@@ -33,34 +36,40 @@ DHT dht(DHTPIN, DHTTYPE);
 #define LED1 25
 #define LED2 26
 
+//Variables globales
 int lectura1;
 int lectura2;
 int estado = 1;
 int contador1;
+
 unsigned long TiempoAhora;
-float t;
 unsigned long TiempoUltimoCambio1 = 0;
 const long Intervalo = 2000;
 const long Intervalo5Seg = 5000;
 unsigned long TiempoUltimoCambio2= 0;
+
 unsigned int VALOR_UMBRAL;
+float t;
 
 void MAQUINA_DE_ESTADOS();
 
 void setup() {
   Serial.begin(115200);
 
+  //DHT inicio
   dht.begin();
 
-  preferences.begin("Eeprom", false);
+  //Eeprom
+  preferences.begin("Eeprom", false); //False significa modo leer y escribir, en True seria solo leer //El "Eeprom" es el nombre del esapcio que se crea
+  VALOR_UMBRAL = preferences.getUInt("VALOR_UMBRAL", 28); //Obtiene el valor //get Uint porque es Usigned int, y el 28 es un valor predeterminado si no hay un "Valor UMBRAL" devuelve 28
 
-  VALOR_UMBRAL = preferences.getUInt("VALOR_UMBRAL", 28);
-
+  //Pinmodes
   pinMode(BOTON1, INPUT);
   pinMode(BOTON2, INPUT);
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
 
+  //Inicio de pantalla
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println(F("SSD1306 allocation failed"));
   }
@@ -73,6 +82,7 @@ void setup() {
 }
 
 void loop() {
+  //Lectura de botones
   lectura1 = digitalRead(BOTON1);
   lectura2 = digitalRead(BOTON2);
 
@@ -87,7 +97,7 @@ void MAQUINA_DE_ESTADOS() {
     case PANTALLA1:
       if (TiempoAhora - TiempoUltimoCambio1 >= Intervalo)  ///delay sin bloqueo
       {
-        TiempoUltimoCambio1 = TiempoAhora;  /// importante actualizar el tiempo
+        TiempoUltimoCambio1 = TiempoAhora;  // importante actualizar el tiempo
 
         display.clearDisplay();
         display.setCursor(0, 20);
@@ -99,23 +109,26 @@ void MAQUINA_DE_ESTADOS() {
         display.print("Valor Umbral: ");
         display.print(VALOR_UMBRAL);
         display.display();
+
       }
       if (t >= VALOR_UMBRAL) {
         digitalWrite(LED1, HIGH);
       } else {
         digitalWrite(LED1, LOW);
       }
+
+      //Para cambiar de estado
       if (lectura1 == PULSADO) {
-        TiempoUltimoCambio2 = TiempoAhora;
+        TiempoUltimoCambio2 = TiempoAhora;  //Se actualiza el tiempo
         estado = ESTADO_CONFIRMACION1;
       }
       break;
 
     case ESTADO_CONFIRMACION1:
-      if (lectura1 == NO_PULSADO && (TiempoAhora - TiempoUltimoCambio2 >= Intervalo5Seg)) {
+      if (lectura1 == NO_PULSADO && (TiempoAhora - TiempoUltimoCambio2 >= Intervalo5Seg)) { //Si pasan 5 segundos
         estado = PANTALLA2;
       }
-      else if (lectura1 == NO_PULSADO){
+      else if (lectura1 == NO_PULSADO){ //Si no pasa 5 segundos vuelve
         estado = PANTALLA1;
       }
       break;
@@ -129,10 +142,6 @@ void MAQUINA_DE_ESTADOS() {
       display.print(VALOR_UMBRAL);
       display.display();
 
-
-      if (lectura1 == PULSADO && lectura2 == PULSADO) {
-        estado = ESTADO_CONFIRMACION2;
-      }
       if (lectura1 == PULSADO) {
         contador1 = 1;
         estado = SUBIR;
@@ -146,7 +155,7 @@ void MAQUINA_DE_ESTADOS() {
 
     case ESTADO_CONFIRMACION2:
       if (lectura2 == NO_PULSADO) {
-        preferences.putUInt("VALOR_UMBRAL", VALOR_UMBRAL);
+        preferences.putUInt("VALOR_UMBRAL", VALOR_UMBRAL);  //Guarda el valor Umbral en una Key llamada "VALOR UMBRAL"
         estado = PANTALLA1;
       }
       break;
@@ -164,7 +173,7 @@ void MAQUINA_DE_ESTADOS() {
       break;
 
     case BAJAR:
-    if (TiempoAhora - TiempoUltimoCambio2 >= Intervalo5Seg) {
+    if (TiempoAhora - TiempoUltimoCambio2 >= Intervalo5Seg) { //Si pasan los 5 segundos va a confirmacion2
         estado = ESTADO_CONFIRMACION2;
       }
       if (lectura2 == NO_PULSADO) {
